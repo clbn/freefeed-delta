1bddb41556609c982d19fedaa52ea40bd6310ffe
Alex Olshansky <alex@creagenics.com>
2020-04-03 00:19:12 +0200

    Fix error caused by archived comments

--- components/Comment.js
+++ components/Comment.js
@@ -3,8 +3,10 @@ import { getFullDate } from '../utils/time';
 
 const Comment = ({ postId, postAuthorId, commentId, comment, users }) => <>
   {comment.body}
-  {' - '}
-  <Link href="/[user]" as={`/${users[comment.authorId].username}`}><a>{users[comment.authorId].displayName}</a></Link>
+  {users[comment.authorId] && <>
+    {' - '}
+    <Link href="/[user]" as={`/${users[comment.authorId].username}`}><a>{users[comment.authorId].displayName}</a></Link>
+  </>}
   {' - '}
   <Link href="/[user]/[post]" as={`/${users[postAuthorId].username}/${postId}#comment-${commentId}`}><a>{getFullDate(comment.createdAt)}</a></Link>
 </>;
