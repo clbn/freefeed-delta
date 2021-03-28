243e68f1fe47ea76f4c59b041292ce322586cc96
Alex Olshansky <alex@creagenics.com>
2021-03-28 14:54:39 +0200

    Add to fetcher "Content-Type: application/json"

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -2,11 +2,12 @@ import fetch from 'isomorphic-unfetch';
 import { parseCookies } from 'nookies';
 
 const fetcher = (url, options, ctx) => {
-  const token = parseCookies(ctx).freefeed_authToken;
+  if (!options) options = {};
+  if (!options.headers) options.headers = {};
+  options.headers['Content-Type'] = 'application/json';
 
+  const token = parseCookies(ctx).freefeed_authToken;
   if (token) {
-    if (!options) options = {};
-    if (!options.headers) options.headers = {};
     options.headers['Authorization'] = `Bearer ${token}`;
   }
 
