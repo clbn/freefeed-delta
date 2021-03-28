b3ca40abda3e7b029408f5e7893b9f223c81b3e3
Alex Olshansky <alex@creagenics.com>
2021-03-28 20:46:50 +0200

    Add "Sign out" (works without JS, too)

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,22 @@
-import Link from 'next/link';
+import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
+import Link from 'next/link';
+import Router from 'next/router';
+import fetch from 'isomorphic-unfetch';
 
 const Header = () => {
   const myUsername = useSelector(state => state.me.username);
+
+  const handleSignout = useCallback(async event => {
+    event.preventDefault();
+    const response = await fetch('/api/signout', { method: 'POST' });
+    if (response.ok) {
+      await Router.push('/');
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  }, []);
 
   return (
     <header>
@@ -15,7 +29,36 @@ const Header = () => {
         <Link href={`/${myUsername}`}>
           <a>{myUsername}</a>
         </Link>
+        {' - '}
+        <form onSubmit={handleSignout} action="/api/signout" method="POST">
+          <input type="hidden" name="redirect" value="1"/>
+          <button type="submit">Sign out</button>
+        </form>
       </>}
+
+      <style jsx>{`
+        form {
+          display: inline;
+          border: none;
+          padding: 0;
+          margin: 0;
+        }
+        button {
+          display: inline;
+          cursor: pointer;
+          color: #1d66bf;
+          background-color: inherit;
+          border: none;
+          font-size: inherit;
+          font-family: inherit;
+          font-weight: inherit;
+          padding: 0;
+          margin: 0;
+        }
+        button:hover {
+          text-decoration: underline;
+        }
+      `}</style>
     </header>
   );
 };
---
+++ pages/api/signout.js
@@ -0,0 +1,28 @@
+import { setCookie } from 'nookies';
+
+export default async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  if (req.method !== 'POST') {
+    res.statusCode = 405;
+    res.end(JSON.stringify({ err: 'Method Not Allowed' }));
+    return;
+  }
+
+  setCookie({ res }, 'freefeed_authToken', '', {
+    maxAge: 30 * 24 * 60 * 60,
+    path: '/',
+    secure: true
+  });
+
+  const redirect = req.body?.redirect; // Used by non-JS clients
+  if (redirect) {
+    res.statusCode = 303;
+    res.setHeader('Location', `${req.headers.origin}/`);
+    res.end();
+    return;
+  }
+
+  res.statusCode = 200;
+  res.end('{}');
+};
