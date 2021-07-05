243773a807c6a97a1df98935e7ef7e92448270e3
Mariia Paraketsova <paraketsova@gmail.com>
2021-07-05 10:49:03 +0200

    Add default userpic in Post.

--- components/Userpic.js
+++ components/Userpic.js
@@ -4,24 +4,7 @@ const Userpic = ({ id, size }) => {
   let url = useSelector(state => id && state.users[id] && state.users[id].userpicUrl);
 
   if (!url) {
-    // TODO: return default SVG userpic
-    return <>
-      <div title="Default userpic">?</div>
-
-      <style jsx>{`
-        div {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          color: #ccc;
-          width: ${size}px;
-          height: ${size}px;
-          border-radius: ${size <= 50 ? 1 : 2}px;
-          box-shadow: 0 0 1px rgba(0, 0, 0, 0.8);
-          margin: 1px;
-        }
-      `}</style>
-    </>;
+    url = '/default-userpic.svg';
   }
 
   if (size <= 50) {
