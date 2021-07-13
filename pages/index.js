bbc55261a091e4346a6b8406f5a24bca6dc8639a
Mariia Paraketsova <paraketsova@gmail.com>
2021-07-13 14:07:36 +0200

    Add PaginationLinks on HomePage

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,15 @@ import { loadHomePage } from '../store/actions';
 import SignIn from '../components/SignIn';
 import DummyPost from '../components/DummyPost';
 import Post from '../components/Post';
+import PaginationLinks from '../components/PaginationLinks';
+import {useRouter} from "next/router";
 
 const IndexPage = () => {
+  const { query: { offset } } = useRouter();
   const myId = useSelector(state => state.me.id);
   const isLoadingPage = useSelector(state => state.isLoadingPage);
   const postIds = useSelector(state => Object.keys(state.posts), shallowEqual);
+  console.log(offset);
 
   if (!myId) {
     return <SignIn/>;
@@ -30,9 +34,14 @@ const IndexPage = () => {
     <main>
       <h1>Home</h1>
 
+      <PaginationLinks offset={offset} username='' hideOnFirst/>
+
       {postIds.map(postId => (
         <Post id={postId} key={postId}/>
       ))}
+
+     <PaginationLinks offset={offset} username=''/>
+
     </main>
   );
 };
