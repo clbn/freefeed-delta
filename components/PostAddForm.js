2d75fb280adc57c0caaf2c906e0cc646bcf58146
Mariia Paraketsova <paraketsova@gmail.com>
2021-07-27 17:03:47 +0200

    Add buttons form for new posts textarea

--- components/PostAddForm.js
+++ components/PostAddForm.js
@@ -1,22 +1,37 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useCallback } from 'react';
 import Textarea from 'react-textarea-autosize';
 
 const PostAddForm = () => {
   const { query: { offset } } = useRouter();
+  const [isExpanded, setExpanded] = useState(false);
+  const handleFocus = useCallback(() => setExpanded(true), [setExpanded]);
+  const handleCancel = useCallback(() => setExpanded(false), [setExpanded]);
+
+  const cancelButton = <a className="cancelButton" onClick={handleCancel}>Cancel</a>;
+  const postButton = <button className="postButton">Post to my feed</button>;
 
   if (offset)  {
     return false;
   }
 
   return (
-    <div className="new-post-area" contentEditable="true">
-    <Textarea
-      minRows={3}
-      maxLength="3000"
-      defaultValue={''}
-      autoFocus
-      //onFocus={startCommenting}
-    />
+    <div className="new-post-area" >
+      <Textarea
+        minRows={3}
+        maxRows={10}
+        maxLength="1500"
+        defaultValue={''}
+        onFocus={handleFocus}
+      />
+      {isExpanded && (
+        <section className="actions">
+          {cancelButton}
+          {postButton}
+        </section>
+      )}
+
       {<style jsx>{`
         textarea {
           display: block;
@@ -26,6 +41,25 @@ const PostAddForm = () => {
           font-size: 1rem;
           padding: 0.25rem;
           margin: 0.125rem 0 0.5rem 0;
+        }
+        .actions {
+          display: flex;
+          justify-content: flex-end;
+          margin-bottom: 1rem;
+          line-height: 1.3rem;
+        }
+        .cancelButton, .postButton {
+          font-size: 0.7rem;
+          padding-right: 0.7rem;
+          vertical-align: middle;
+        }
+        .postButton {
+          color: inherit;
+          font-weight: normal;
+          background-color: inherit;
+          border: solid 0.1rem #999999;
+          padding: 0.1rem 0.2rem;
+          margin: 0;
         }
       `}</style>}
     </div>
